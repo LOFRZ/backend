8cb9d467fcf327d6368642397ee57e577d4479fe
LOFRZ <almenzo.arroyo@gmail.com>
2024-09-17 03:22:04 +0200

    fix signUp

--- djangobnb_backend/djangobnb_backend/settings.py
+++ djangobnb_backend/djangobnb_backend/settings.py
@@ -55,7 +55,24 @@ CORS_ALLOWED_ORIGINS = [
     'http://127.0.0.1:3000',
 ]
 
-CORS_ALLOW_ORIGINS = True
+
+CSRF_TRUSTED_ORIGINS = [
+    'http://127.0.0.1:8000',
+    'http://127.0.0.1:3000',
+]
+
+CORS_ORIGINS_WHITELIST = [
+    'http://127.0.0.1:8000',
+    'http://127.0.0.1:3000',
+]
+
+
+CORS_ALLOW_ALL_ORIGINS = True
+
+
+CORS_ALLOW_HEADERS = "*"
+
+
 
 REST_AUTH = {
     "USE_JWT": True,
