157fb68a14e576ef8e2a068d8c066dbd2633dfbd
LOFRZ <almenzo.arroyo@gmail.com>
2024-10-28 22:46:57 +0100

    chat

--- djangobnb_backend/chat/models.py
+++ djangobnb_backend/chat/models.py
@@ -1,3 +1,19 @@
+import uuid
+
 from django.db import models
 
-# Create your models here.
+from useraccount.models import User
+
+class Conversation(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    users = models.ManyToManyField(User, related_name=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(auto_now=True)
+
+class ConversationMessage(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    conversation = models.ForeignKey(Conversation, related_name='message', on_delete=models.CASCADE)
+    body = models.TextField()
+    sent_to = models.ForeignKey(User, related_name='received_messages', on_delete=models.CASCADE)
+    created_by = models.ForeignKey(User,related_name='sent_message', on_delete=models.CASCADE)
+    created_at = models.DateField(auto_now_add=True)
