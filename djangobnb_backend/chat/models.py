f4efb51e1a950f31105f043713dc78a86de3ce5c
LOFRZ <almenzo.arroyo@gmail.com>
2024-10-28 23:20:23 +0100

    mp

--- djangobnb_backend/chat/models.py
+++ djangobnb_backend/chat/models.py
@@ -4,16 +4,18 @@ from django.db import models
 
 from useraccount.models import User
 
+
 class Conversation(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    users = models.ManyToManyField(User, related_name=True)
+    users = models.ManyToManyField(User, related_name='conversations')
     created_at = models.DateTimeField(auto_now_add=True)
     modified_at = models.DateTimeField(auto_now=True)
 
+
 class ConversationMessage(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    conversation = models.ForeignKey(Conversation, related_name='message', on_delete=models.CASCADE)
+    conversation = models.ForeignKey(Conversation, related_name='messages', on_delete=models.CASCADE)
     body = models.TextField()
     sent_to = models.ForeignKey(User, related_name='received_messages', on_delete=models.CASCADE)
-    created_by = models.ForeignKey(User,related_name='sent_message', on_delete=models.CASCADE)
-    created_at = models.DateField(auto_now_add=True)
+    created_by = models.ForeignKey(User, related_name='sent_messages', on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
