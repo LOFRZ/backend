cd26de13a47ce5812a25ddf449c4fefa0f1ed304
LOFRZ <almenzo.arroyo@gmail.com>
2025-07-15 17:11:04 +0200

    Update serializers.py

--- djangobnb_backend/property/serializers.py
+++ djangobnb_backend/property/serializers.py
@@ -1,9 +1,8 @@
 from rest_framework import serializers
 
 from .models import Property, Reservation
-
-
 from useraccount.serializers import UserDetailSerializer
+
 
 class PropertiesListSerializer(serializers.ModelSerializer):
     image_url = serializers.SerializerMethodField()
@@ -23,8 +22,10 @@ class PropertiesListSerializer(serializers.ModelSerializer):
             return request.build_absolute_uri(obj.image.url)
         return None
 
+
 class PropertiesDetailSerializer(serializers.ModelSerializer):
     landlord = UserDetailSerializer(read_only=True, many=False)
+    image_url = serializers.SerializerMethodField()
 
     class Meta:
         model = Property
@@ -40,11 +41,23 @@ class PropertiesDetailSerializer(serializers.ModelSerializer):
             'landlord'
         )
 
+    def get_image_url(self, obj):
+        request = self.context.get('request')
+        if obj.image and hasattr(obj.image, 'url'):
+            return request.build_absolute_uri(obj.image.url)
+        return None
+
+
 class ReservationsListSerializer(serializers.ModelSerializer):
     property = PropertiesListSerializer(read_only=True, many=False)
 
     class Meta:
         model = Reservation
         fields = (
-            'id', 'start_date', 'end_date', 'number_of_nights', 'total_price', 'property'
+            'id',
+            'start_date',
+            'end_date',
+            'number_of_nights',
+            'total_price',
+            'property',
         )
