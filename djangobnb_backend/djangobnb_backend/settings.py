2c452b82ab7648ab527ae03fb03128afa51443c7
FIRST_NAME LAST_NAME <l.frz65@gmail.com>
2025-07-16 03:08:43 +0200

    cors

--- djangobnb_backend/djangobnb_backend/settings.py
+++ djangobnb_backend/djangobnb_backend/settings.py
@@ -72,8 +72,8 @@ MEDIA_URL = '/media/'
 # Middleware
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
     'corsheaders.middleware.CorsMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -166,15 +166,17 @@ USE_TZ = True
 
 # CORS
 CORS_ALLOWED_ORIGINS = [
-    'http://127.0.0.1:8000',
-    'http://127.0.0.1:3000',
+    'https://airbnb-clone-g42s.vercel.app',  # ton frontend déployé
+    'http://localhost:3000',                 # pour le dev local
 ]
 CSRF_TRUSTED_ORIGINS = [
     'http://127.0.0.1:8000',
     'http://127.0.0.1:3000',
+    'https://airbnb-clone-g42s.vercel.app',
 ]
-CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOW_CREDENTIALS = True
 CORS_ALLOW_HEADERS = "*"
+
 
 # Auto primary key
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
